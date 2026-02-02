0301b21c0c45d6a590168cb10a016c9fbb74d517
JBeraldo <joaoangelo409@gmail.com>
2026-02-02 01:13:45 -0300

    Cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from 'hono';
 import { createReceiptRoutes } from './routes/receipt';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
+
+app.use(cors({
+    origin: '*',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ['POST','PUT','DELETE','PATCH', 'GET', 'OPTIONS'],
+    maxAge: 600,
+    credentials: true,
+  }))
 // Health check
 app.get('/', (c) => {
   return c.json({ message: 'Server is running' });
